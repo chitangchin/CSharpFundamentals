1375b307508e77564a598f758efd1051cd537522
chitangchin <chitangb.chin@gmail.com>
2024-08-27 09:05:11 -0400

    Type System: Floating Type

--- Fundamentals/TypeSystem/FloatingTypes/FloatingTypes.cs
+++ Fundamentals/TypeSystem/FloatingTypes/FloatingTypes.cs
@@ -4,7 +4,24 @@
     {
         public static string Solution()
         {
-            return "Yes!";
+            //Float can be implicitly converted to double
+            //Integral types can be converted to decimal
+            //Decimal cannot be turned into float or double, you must convert atleast one variable to decimal to perform operations
+            double a = 1.0;
+            decimal b = 2.1m;
+            float f = 123.45E-2f;//exponent to -2 moves the decimal place twice forward
+            double db = 1.111e2;//Outputs to 111.1 moves decimal placed twice forward
+            decimal dl = 1.5E6m;//Outputs expoent 10^6 moves decimal place back 6 times: 1500000
+
+            /*
+             * Decimal: E(negative or none)(number)m
+             * Double: e(negative or none)(number)
+             * float: E(negative or none)(number)f
+             * 
+             * ONLY One implicit conversion available: float to double
+             */
+
+            return $"Decimal Arithmetic: {(decimal)a + b}, {a + (double)b}, {f}, {db}, {dl}";
         }
     }
 }
--- FundamentalsTest/TypeSystem/FloatingTypesTest.cs/FloatingTypesTest.cs
+++ FundamentalsTest/TypeSystem/FloatingTypesTest.cs/FloatingTypesTest.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Fundamentals.TypeSystem.FloatingTypes;
 
 namespace FundamentalsTest.TypeSystem.FloatingTypesTest.cs
 {
-    internal class FloatingTypesTest
+    public class FloatingTypesTest
     {
+        private readonly string expected = "Decimal Arithmetic: 3.1, 3.1, 1.2345, 111.1, 1500000";
+        [Test]
+        public void Test()
+        {
+            Assert.That(FloatingTypes.Solution(), Is.EqualTo(expected));
+        }
     }
 }
